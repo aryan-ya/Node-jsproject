4c3e9591a5f1c3f142f7b43f820091d31a09febd
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-07-17 14:11:31 +0530

    update

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 const express = require("express");
 
-const app =  express()
-const bcrypt = require("bcrypt")
+const app = express()
+const bcrypt = require("bcrypt")  //important bcrypt
+const users = []
 
-app.get('/' ,(req , res) => {
-  res.render("index.ejs")  
-})
-app.get('/login' ,(req , res) => {
-    res.render("login.ejs")  
-  })
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/register' ,(req , res) => {
-    res.render("register.ejs")  
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+    console.log(users);   //display newly registered in this console
+    res.redirect("/login")
+  } catch (e){
+    console.log(e);
+    res.redirect("/register")
+
+  }
 })
-  
+
+
+// routes
+app.get('/', (req, res) => {
+  res.render("index.ejs")
+})
+app.get('/login', (req, res) => {
+  res.render("login.ejs")
+})
+
+app.get('/register', (req, res) => {
+  res.render("register.ejs")
+})
+console.log(users); // display newly registered in consloe 
 app.listen(3000);
